3a027769d20bed15e69990f1f42a46634043fb20
thermalbarker <thermalbarker@users.noreply.github.com>
2022-11-10 20:46:47 +0100

    Forgot getName

--- sml.py
+++ sml.py
@@ -50,6 +50,9 @@ class SmlListEntry:
         self.value = value
         self.valueSignature = valueSignature
 
+    def getName(self):
+        return self.objName
+        
     def getValue(self):
         return float(self.value) * 10 ** self.scalar if self.unit is not None else self.value
     
